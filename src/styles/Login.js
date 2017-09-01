b349f2f8de93567d42fe547ffd5186d1c21b3788
aaloui <a.aloui@rustica.fr>
2017-09-01 17:03:48 +0200

    minor optimisation

---
+++ src/styles/Login.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    containerLogin: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#00bde6'
+    },
+    errorTextStyle: {
+        color: '#E64A19',
+        alignSelf: 'center',
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+});
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import {View, Button} from 'react-native'
 import {LabelTextField} from '../components/LabelTextFiel'
+import loginStyles from '../styles/Login'
 
 export default class Login extends React.Component{
 
     constructor(props) {
         super(props)
-
         this.state = { email: '', password: '', error: '', loading: false };
-
     }
 
     handleSubmit() {
@@ -19,7 +18,7 @@ export default class Login extends React.Component{
 
     render(){
         return(
-            <View>
+            <View style={loginStyles.containerLogin}>
                 <LabelTextField
                     label='Fucking mail'
                     value={this.state.email}
